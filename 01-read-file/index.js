9a57c1c0ed3cbf267aba29128d9addafa828ceb7
denismezhenin <103366013+denismezhenin@users.noreply.github.com>
2022-11-07 19:58:39 +0300

    feat: compleate first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+const dirname = path.join(__dirname, 'text.txt')
+const stream = fs.createReadStream(dirname, 'utf-8')
+stream.on('data', data => console.log(data))
+// console.log(dirname)
