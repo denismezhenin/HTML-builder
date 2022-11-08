9df0fa0ea5af35297b7e2d787201e6c7d344eb2d
denismezhenin <dzianismiazhenin@gmail.com>
2022-11-08 14:17:18 +0300

    fix: problem with styles.css in 6 task and refacoring previous tasks

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -5,10 +5,10 @@ const output = fs.createWriteStream(path.join(__dirname, 'text.txt'))
 stdout.write('Please write some text\n')
 stdin.on('data', data => {
 	if (data.toString().trim() == 'exit') {
+		stdout.write('Goodbye!\n')
 		process.exit()
 	}
 	output.write(data)
-	process.on('exit', () => stdout.write('Goodbye!\n'));
 })
 
 process.on('SIGINT', () => {
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,34 +1,35 @@
-console.log('постараюсь сегодня доделать')
 const fs = require('fs')
 const path = require('path')
 const filesPath = path.join(__dirname, 'assets')
 const filesCopyPath = path.join(__dirname, 'project-dist', 'assets')
+const bundleFilePath = path.join(__dirname, 'project-dist', 'style.css')
+const stylesPath = path.join(__dirname, 'styles')
 
-// fs.mkdir(filesCopyPath, { recursive: true }, error => {
-// 	if (error)
-// 		throw error
-// })
-	// fs.rm(filesCopyPath, {force: true, recursive: true}, error => {
-	// 	if (error) throw error;
-	
-	// })
 	async function del() {
 	await fs.rm(filesCopyPath, {force: true, recursive: true}, error => {
 		if (error) throw error;
-		copyFiles(filesPath, filesCopyPath)
+		copyFiles(filesPath, filesCopyPath)	
 	})
 	}
 	del()
 
-
-
 	async function copyFiles(src, dest) {
-		// await fs.rm(dest, {force: true, recursive: true}, error => {
-		// 	if (error) throw error;
-		
-		// })
 		fs.mkdir(dest, { recursive: true }, error => {
 			if (error) throw error;
+			fs.writeFile(path.join(__dirname, 'project-dist', 'style.css'), "", (error) => {
+				if (error) throw error;
+			})
+			const output = fs.createWriteStream(bundleFilePath)
+			fs.readdir(stylesPath, (error, files) => {
+				if (error) throw error;
+				files.forEach(file => {
+					const fileExt = path.extname(path.join(__dirname, 'styles', file))
+				if(fileExt == '.css') {
+				const input = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8')
+				input.pipe(output)
+				}
+				})
+				})
 			fs.readdir(src, {withFileTypes: true}, (error, item) => {
 				if (error) throw error;
 				item.forEach(item => {
@@ -42,86 +43,16 @@ const filesCopyPath = path.join(__dirname, 'project-dist', 'assets')
 							}
 						})
 					}
-	
 				})
 			})
 		})
-	}
-
-
-// const bundleFilePath = path.join(__dirname, 'project-dist', 'style.css')
-// const stylesPath = path.join(__dirname, 'styles')
-
-async function deleteStyle() {
-	// await fs.rm(bundleFilePath, {force: true, recursive: true}, error => {
-	// 	console.log(1)
-	// 	if (error) throw error;
 		
-	// })
-	createStyle() 
-}
-
-async function createStyle() {
-	fs.writeFile(bundleFilePath, "", (error) => {
-		console.log(1)
-		if (error) throw error;
-	})
-	console.log(1)
-	const output = fs.createWriteStream(bundleFilePath)
-	
-
-//  fs.readdir(stylesPath, (error, files) => {
-// 	if (error) throw error;
-// 	files.forEach(file => {
-// 		const fileExt = path.extname(path.join(__dirname, 'styles', file))
-// if(fileExt == '.css') {
-// const input = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8')
-// input.pipe(output)
-// }
-// 	})
-// })
-}
-// deleteStyle()
-const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css')
-const stylesPath = path.join(__dirname, 'styles')
-
-// fs.rm(bundleFilePath, {force: true, recursive: true}, error => {
-// 	if (error) throw error;
-	
-// })
-fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), " ", (error) => {
-	console.log(1)
-	if (error) throw error;
-})
-// const output = fs.createWriteStream(bundleFilePath)
-
-// fs.readdir(stylesPath, (error, files) => {
-// if (error) throw error;
-// files.forEach(file => {
-// 	const fileExt = path.extname(path.join(__dirname, 'styles', file))
-// if(fileExt == '.css') {
-// const input = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8')
-// input.pipe(output)
-// }
-// })
-// })
-
-
-
+	}
 
 const htmlFilePath = path.join(__dirname, 'project-dist', 'index.html')
 const templateFilePath = path.join(__dirname, 'template.html')
 let temp
 
-// fs.rm(bundleFilePath, {force: true, recursive: true}, error => {
-// 	if (error) throw error;
-// 	fs.writeFile(htmlFilePath, "", (error) => {
-// 		if (error) throw error;
-// 	})
-// 	const input = fs.createReadStream(templateFilePath)
-// 	const output = fs.createWriteStream(htmlFilePath)
-// 	input.pipe(output)
-// })
 fs.readFile(templateFilePath, 'utf8', (error, data) => {
 	temp = data
 })
@@ -129,7 +60,6 @@ fs.readFile(templateFilePath, 'utf8', (error, data) => {
 fs.readdir(path.join(__dirname, 'components'), {withFileTypes: true}, (error, files) => {
 	files.forEach(file => {
 		let fileName = path.parse(path.join(__dirname, file.name)).name
-		// console.log(fileName)
 		let component 
 		fs.readFile(path.join(__dirname, 'components', file.name), 'utf8', (err, data) => {
 			if (err) throw err;
@@ -137,45 +67,11 @@ fs.readdir(path.join(__dirname, 'components'), {withFileTypes: true}, (error, fi
 			temp = temp.replace(`{{${fileName}}}`, component)
 			fs.rm(bundleFilePath, {force: true, recursive: true}, error => {
 				if (error) throw error;
-				// console.log(typeof temp)
 				fs.writeFile(htmlFilePath, temp, (error) => {
 					if (error) throw error;
 				})
-				// const input = fs.createReadStream(templateFilePath)
-				// const output = fs.createWriteStream(htmlFilePath)
-				// input.pipe(output)
 			})
-			// fs.writeFile()
-			// console.log(temp)
-// console.log(component)
 		})
-
 	})
 	
 })
-// copyFiles(filesPath, filesCopyPath)
-// copyFiles(filesPath, filesCopyPath)
-
-
-
-
-
-
-// input.on('data', data => {
-
-// })
-
-
-// fs.readdir(stylesPath, (error, files) => {
-// 	if (error) throw error;
-// 	files.forEach(file => {
-// 		const fileExt = path.extname(path.join(__dirname, 'styles', file))
-// if(fileExt == '.css') {
-// const input = fs.createReadStream(path.join(__dirname, 'styles', file), 'utf-8')
-// input.pipe(output)
-// }
-// 	})
-// })
-
-
-
